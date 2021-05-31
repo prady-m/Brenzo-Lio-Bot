19211af62fbdbbfb9404f13161138c7481c74068
DevelopedBots <83950041+DevelopedBots@users.noreply.github.com>
2021-05-31 12:51:46 +0530

    Update __main__.py

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -73,11 +73,13 @@ I am powerfull telegram group manager bot and I can help you to control your cha
 buttons = [
     [
         InlineKeyboardButton(text="❓ Information ❓", callback_data="aboutmanu_"),
-    ],
-    [
+
+    ),
         InlineKeyboardButton(text="⚙️ Commands Help ⚙️", callback_data="help_back"),
-    ],
-    [
+    ),
+    
+  ],
+  [
         InlineKeyboardButton(
             text="➕ Add me to Your chat ➕", url="http://t.me/BrenzoLio_Bot?startgroup=true"
         ),
